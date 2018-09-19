a96b04fc1d2d94ae73bd35e8af2e295c9c0986fa
chenjiaying <719121095@qq.com>
2018-09-20 00:21:43 +0800

    # 模拟误操作

--- src/main/java/com/wing/controller/HelloController.java
+++ src/main/java/com/wing/controller/HelloController.java
@@ -5,41 +5,4 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping("hello")
-    public String hello(String name){
-        //test the git
-        return "hello" + name + "   springboot!";
-    }
-
-    public void doSomething(){
-        System.out.println("doSomething");
-    }
-
-    public void sayHello(){
-        //增加一个方法
-        System.out.println("hello");
-    }
-
-    public void doA(){
-        System.out.println("A");
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public void doB(){
-        int a  = 1;
-        System.out.println(a);
-    }
 }
